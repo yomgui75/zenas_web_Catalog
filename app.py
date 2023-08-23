6d62cb38b532e14d834feb7be98a35000985cc5d
yomgui75 <yomgui75@gmail.com>
2023-08-23 11:32:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,10 @@ import streamlit
 import snowflake.connector
 streamlit.title('My Parent new healthy dinner')
 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),
+CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
