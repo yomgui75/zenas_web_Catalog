0139576387e29b407aaec0e144dd91e8eaaa2707
yomgui75 <yomgui75@gmail.com>
2023-08-23 11:36:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import streamlit
 import snowflake.connector
 import pandas
+
 streamlit.title('Zena\'s Amazing Athleisure Catalog')
+
 # connect to snowflake
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -10,6 +12,7 @@ my_cur.execute("select color_or_style from catalog_for_website")
 my_catalog = my_cur.fetchall()
 # put the dafta into a dataframe
 df = pandas.DataFrame(my_catalog)
+
 # temp write the dataframe to the page so I Can see what I am working with
 # streamlit.write(df)
 # put the first column into a list
@@ -22,14 +25,9 @@ product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
 # use the option selected to go back and get all the info from the database
 
 
-my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where
-color_or_style = '" + option + "';")
+my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where color_or_style = '" + option + "';")
 df2 = my_cur.fetchone()
-streamlit.image(
-df2[0],
-width=400,
-caption= product_caption
-)
+streamlit.image(df2[0], width=400, caption= product_caption )
 streamlit.write('Price: ', df2[1])
 streamlit.write('Sizes Available: ',df2[2])
 streamlit.write(df2[3])
